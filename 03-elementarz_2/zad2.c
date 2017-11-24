b1086c20b3b0855556fe2d92a00bbf124059e079
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2017-11-25 00:28:01 +0100

    Update zad2.c

--- 03-elementarz_2/zad2.c
+++ 03-elementarz_2/zad2.c
@@ -1,15 +1,14 @@
+/*2. Zamiast komentarzy / ... / w programie poniżej wpisz kod tak,
+ aby powstał program, który sprawdza czy podana liczba całkowita jest pierwsza. */
+ 
 #include <stdio.h>
-
 int main() {
-  int i, n, jest_pierwsza;
+  int n,jest_pierwsza,i;
 
   printf("Podaj liczbe calkowita: ");
   scanf("%d", &n);
-
-  for (i = 10; i <= n; i++,)
-     {
+   for (i = 10; i <= n; i++) {
       jest_pierwsza = 0;
-    }
-  printf("Podana liczba '%d' jest %s\n", n, jest_pierwsza ? "pierwsza" : "złożona");
+  }
+ printf("Podana liczba '%d' jest %s\n", n, jest_pierwsza ? "pierwsza" : "złożona");
 }
-
