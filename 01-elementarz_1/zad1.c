5328e2d5ac9822c840ffe6edaf611193b46eb25b
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2017-11-24 18:05:24 +0100

    Update zad1.c

---
+++ 01-elementarz_1/zad1.c
@@ -0,0 +1,15 @@
+/*Napisz program obliczający wartość każdego z poniższych wyrażeń:
+
+a. n1=5+3*8/2-3
+b. n2=2%2+2*2-2/2
+c. n3=2*4*(5+9/2)
+*/
+#include <stdio.h>
+int main() {
+
+int n1=5+3*8/2-3;
+int n2=2%2+2*2-2/2;
+int n3=2*4*(5+9/2);
+
+printf("%d \n %d \n %d", n1, n2, n3);
+}
