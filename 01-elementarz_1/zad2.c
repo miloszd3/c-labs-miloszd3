dfc8d69972ea424b76c6bea8bef5a2f7fd1487a8
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2017-11-24 20:10:42 +0100

    Update zad2.c

---
+++ 01-elementarz_1/zad2.c
@@ -0,0 +1,10 @@
+/* Program ten wypisuje liczby z tablicy tabela[]
+   w odwrotnej kolejności: 12, 6, 4, 2, 1 */
+   #include <stdio.h>
+int main() {
+   int tabela[] = {1, 2, 4, 6, 12};
+   for (int i=4;i>=0;i--)
+   {
+     printf("%d \n",tabela[i]);
+   }
+}
