7153a2dcd1edd46954cbd0ecf4d3433520cc579c
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2017-11-25 10:01:06 +0100

    Update zad2.c

--- 03-elementarz_2/zad2.c
+++ 03-elementarz_2/zad2.c
@@ -8,6 +8,7 @@ int main() {
   printf("Podaj liczbe calkowita: ");
   scanf("%d", &n);
    for (i = 2; i*i <= n; i++) {
+     if (n%i == 0)
       jest_pierwsza = 0;
   }
  printf("Podana liczba '%d' jest %s\n", n, jest_pierwsza ? "pierwsza" : "złożona");
