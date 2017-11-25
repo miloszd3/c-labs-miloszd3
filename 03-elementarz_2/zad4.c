181f66ff5cd852c235031e90b67ec5c0bd33bf8d
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2017-11-25 10:47:17 +0100

    Update zad4.c

--- 03-elementarz_2/zad4.c
+++ 03-elementarz_2/zad4.c
@@ -8,10 +8,12 @@ main() {
     nb = 0;
     nl = 0;
     nt = 0;
-    while((c = getchar()) != '\n' && c != EOF){
+    printf("'Esc', a potem 'Enter'by zakończyć zliczanie) \n\n");
+    while((c = getchar()) != '\0' && c != EOF){
         if (c == ' ') ++nb;
         else if (c == '\n') ++nl;
         else if (c == '\t') ++nt;
+        else if (c == 27) break;
     }
    printf("liczba znaków: \n Odstępu = %d \n Tabulacji = %d \n Nowego wiersza = %d \n", nb, nt, nl );
 }
