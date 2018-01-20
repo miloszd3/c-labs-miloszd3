d17356d96b344e920cb7dc7a13bc287246700b7b
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2018-01-20 09:55:29 +0100

    Update zad4.c

--- 02-petle/zad4.c
+++ 02-petle/zad4.c
@@ -1,20 +1,19 @@
 /* Napisz program, który wyliczy sumę liczb naturalnych od 1 do liczby n podanej przez użytkownika.
 Kiedy użytkownik poda liczbę mniejszą niż 1, to program powinien wypisać stosowny komunikat o błędzie. */
 
-
 #include <stdio.h>
 
 int main (){
-int liczba, suma=0;
-printf ("Podaj liczbe :");
-scanf ("%d", &liczba);
-
-if (liczba<1)
-printf ("Podana liczba musi byc wieksza od 0");
-else {
-  for(int i=1; i<=liczba; i++){
-    suma+=i;
+  int liczba, suma=0;
+  printf ("Podaj liczbe :");
+  scanf ("%d", &liczba);
+  
+  if (liczba<1)
+  printf ("Podana liczba musi byc wieksza od 0");
+  else {
+    for(int i=1; i<=liczba; i++){
+      suma+=i;
     }
-  printf("Suma liczb to :%d\n",suma);
+    printf("Suma liczb to :%d\n",suma);
   }
 }
