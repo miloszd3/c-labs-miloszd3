d86debc771192fdac51f7faa174d1eca814670f4
miloszd3 <33776174+miloszd3@users.noreply.github.com>
2018-01-20 09:52:29 +0100

    Update zad4.c

--- 02-petle/zad4.c
+++ 02-petle/zad4.c
@@ -12,11 +12,9 @@ scanf ("%d", &liczba);
 if (liczba<1)
 printf ("Podana liczba musi byc wieksza od 0");
 else {
-
   for(int i=1; i<=liczba; i++){
     suma+=i;
     }
   printf("Suma liczb to :%d\n",suma);
-
   }
 }
